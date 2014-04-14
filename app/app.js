61eb1a9e06cd119924a4c8dd3c57a59836f2c7c8
e <eric345@gmail.com>
2014-04-14 12:48:54 -0500

    clean up before sequelize

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ app.configure('dev', function(){
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "Connection error: "));
 db.once('open', function(){
-  console.log('opened');
+  //console.log('opened');
 })
 
 require('./routes.js')(app);
--- test/models/company_spec.js
+++ test/models/company_spec.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-	User = require('../../app/models/company.js'),
+	Company = require('../../app/models/company.js'),
+	User = require('../../app/models/user.js'),
 	should = require('should');
 
 
@@ -27,7 +28,71 @@ describe('The company model', function(){
 	 	}
 	});
 
-	it('should should exist', function(done){
+
+	it('should should exist', function(){
+		Company.should.be.ok;
+	});	
+
+	describe('validations', function(){
+		var company;
+		beforeEach(	function(){
+			company = new Company();
+		})
+
+		it('should require a name', function(done){
+			company.validate(function(err){
+				err.should.be.ok;
+				err.errors.name.type.should.eql('required');
+				done();
+			});
+		});
+	});
+
+	describe('Users subdocument', function(){
+		var user1, user2, company;
+		
+		beforeEach(function(done){
+			var usr1 = new User({
+				name: 'Eric',
+				email: 'e@gmail.com',
+				password: 'password'
+			});
+
+			var usr2 = new User({
+				name: 'James',
+				email: 'j@gmail.com',
+				password: 'password'
+			})
+			company = new Company();
+			company.name = "ABC";
+			usr1.save(function(err){
+				if(err) return done(err)
+				usr2.save(function(err){
+					if(err) return done(err);
+					company.users.push({user: usr1._id, role: 'admin'});
+					company.save(function(err){
+						if(err) return done(err);
+						//console.log(company)
+						done();
+					})
+				})
+			})
+		});
+
+		it('should find users who are admins', function(done){
+			Company.findOne({name: 'ABC'})
+				.populate({
+					path: 'users.user',
+					match: {email: 'e@gmail.com'}
+				})
+				.exec(function(err, company){
+					if(err) return done(err);
+					console.log(company.users[0]);
+					done();
+				})
+		});
+
+		//it('should')
 
 	})
 
