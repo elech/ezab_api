b0a79532212a73e9e5cc17c8841e74193eae0065
e <eric345@gmail.com>
2014-04-17 16:37:07 -0500

    token create

--- app/controllers/tokens.js
+++ app/controllers/tokens.js
@@ -1,8 +1,23 @@
 var jwt = require('jsonwebtoken');
 module.exports = function(app){
-	var User = app.get('models').User;
+
 	function _create(){
-		User.findOne({})
+		User.find({where: {email: req.body.email}}).then(function(user){
+			if(!user) return res.send(401);
+			user.comparePassword(req.body.password).then(function(match){
+				if(!match) return res.send(401);
+				var profile = {
+					name: user.get('name'),
+					email: user.get('email')
+				};
+				var token = jwt.sign(profile, secret, { expiresInMinutes: 60*5 });
+				res.send(200, token);
+			}, function(){
+				return res.send(401);
+			})
+		}, function(err){
+			return res.send(500);
+		})
 	}
 	return {
 		create: _create
